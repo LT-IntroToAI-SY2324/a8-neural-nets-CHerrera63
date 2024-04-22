3a731d347c472a35fdb47d01221a7a1ef07f91cd
CHerrera63 <cherrera63@cps.edu>
2024-04-22 10:11:44 -0500

    Xor Data

--- a8.py
+++ a8.py
@@ -1,7 +1,28 @@
 from neural import *
+# train_data = [
+#     ([0, 0], [0]),
+#     ([0, 1], [1]),
+#     ([1, 0], [1]),
+#     ([1, 1], [0]),
+# ]
 
+
+
+# print(gtn.evaluate([0, 0]))
+# print(gtn.evaluate([0, 1]))
+# print(gtn.evaluate([1, 0]))
+
+xor_data = [
+    ([0, 0], [0]),
+    ([0, 1], [1]),
+    ([1, 0], [1]),
+    ([1, 1], [0]),
+]
+xor_nn = NeuralNet(2, 2, 1)
+xor_nn.train(xor_data, iters = 1000, print_interval= 100)
+print(xor_nn.test_with_expected(xor_data))
+print(xor_nn.evaluate([1, 1]))
 print("<<<<<<<<<<<<<< XOR >>>>>>>>>>>>>>\n")
-
 
 
 
